078233493b0fafd0919167a159bfcba6590ece9e
6ens <95295001+6ens@users.noreply.github.com>
2021-11-30 18:12:51 +0100

    Merge remote-tracking branch 'origin/main'

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,63 @@
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
 //        //1
-//        int op1 = 4;
-//        int op2 = 5;
-//        String operator = "/";
+//        Scanner scanner = new Scanner(System.in);
+//        scanner.useLocale(Locale.ENGLISH);
+//        double op1 = scanner.nextDouble();
+//        double op2 = scanner.nextDouble();
+//        String operator = scanner.nextLine().replaceAll(" ","");
 //        System.out.println(op1 + " " + operator + " " + op2 + " = " + calculate(op1, op2, operator));
-//
+
+//        //2
 //        Scanner scanner = new Scanner(System.in);   //erstelle einen neuen "Scanner"
 //        int n = scanner.nextInt();                  //lies Text bis zum Zeilenumbruch
-//        //2
 //        System.out.println("fib: " + fib(n));
 //
-//        //3
-//        Scanner scanner = new Scanner(System.in);
-//        ArrayList<Integer> eingaben = new ArrayList<>();
-//        while (true) {
-//            try {
-//                int e = scanner.nextInt();
-//                eingaben.add(e);
-//                System.out.println("Neue Eingabe: " + eingaben.get(eingaben.size() - 1));
-//            } catch (Exception e) {
-//                break;
-//            }
-//        }
-//        int[] eingabe_array = new int[eingaben.size()];
-//        for (int i = 0; i < eingaben.size(); i++) {
-//            eingabe_array[i] = eingaben.get(i);
-//        }
-//        System.out.println("Eingabe abgebrochen\n");
-//        System.out.println("Summe: " + sum(eingabe_array, eingaben.size()));
-//
+        //3
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> eingaben = new ArrayList<>();
+        while (true) {
+            try {
+                int e = scanner.nextInt();
+                eingaben.add(e);
+                System.out.println("Neue Eingabe: " + eingaben.get(eingaben.size() - 1));
+            } catch (Exception e) {
+                break;
+            }
+        }
+        int[] eingabe_array = new int[eingaben.size()];
+        for (int i = 0; i < eingaben.size(); i++) {
+            eingabe_array[i] = eingaben.get(i);
+        }
+        System.out.println("Eingabe abgebrochen\n");
+        System.out.println("Summe: " + sum(eingabe_array));
+
         //4
-        String[] elemente = new String[]{"Hallo", " Ich", " Bins"};
-        System.out.println(concat(elemente));
+//        String[] elemente = new String[]{"Hallo", " Ich", " Bins"};
+//        System.out.println(concat(elemente));
     }
 
     //Übung 7:
     //1
     public static double calculate(double op1, double op2, String operator) {
-        if (operator == "+") {
+        if (operator.equals("+")) {
             return (op1 + op2);
         }
-        if (operator == "-") {
+        if (operator.equals("-")) {
             return (op1 - op2);
         }
-        if (operator == "*") {
+        if (operator.equals("*")) {
             return (op1 * op2);
         }
-        if (operator == "/") {
+        if (operator.equals("/")) {
             return (op1 / op2);
         }
-        if (operator == "%") {
+        if (operator.equals("%")) {
             return (op1 % op2);
         } else System.out.println("Kein gueltiger Operator");
         return 0;
@@ -70,9 +73,9 @@ public class Main {
     }
 
     //3
-    public static int sum(int[] werte, int length) {
+    public static int sum(int[] werte) {
         int summe = 0;
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < werte.length; i++) {
             summe += werte[i];
         }
         return summe;
@@ -80,6 +83,7 @@ public class Main {
 
     //4
     public static String concat(String[] elemente) {
+        //TODO
         String result = "";
         String[] satz = new String[2];
 
