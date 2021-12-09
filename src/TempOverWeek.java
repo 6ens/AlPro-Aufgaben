f0e390df70d63a8f7e1bfc5cd16beb66afc6e026
6ens <95295001+6ens@users.noreply.github.com>
2021-12-09 18:21:04 +0100

    Initial commit

--- src/TempOverWeek.java
+++ src/TempOverWeek.java
@@ -8,6 +8,7 @@ public class TempOverWeek {
 
     int[][] temperaturesOverTheWeek;
     String[] wochentage = {"Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"};
+    ArrayList<String> ArrayListWochentage = new ArrayList<>(Arrays.asList(wochentage));
 
 
     public void minMaxTempTag(int tag) throws Exception {
@@ -67,14 +68,15 @@ public class TempOverWeek {
     }
 
     public void setVal(String tag, int stunde, int wert) {
-        ArrayList<String> arr = new ArrayList<>(Arrays.asList(wochentage));
-        int tagInt = arr.indexOf(tag);
-        System.out.println(tagInt);
+        int tagInt = ArrayListWochentage.indexOf(tag);
         this.temperaturesOverTheWeek[tagInt][stunde] = wert;
     }
 
     public void setVals(String tag, int[] werte) {
-
+        int tagInt = ArrayListWochentage.indexOf(tag);
+        for (int stunde = 0; stunde < this.temperaturesOverTheWeek[tagInt].length; stunde++) {
+            this.temperaturesOverTheWeek[tagInt][stunde] = werte[stunde];
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -88,12 +90,18 @@ public class TempOverWeek {
                 {9, 9, 9, 9, 9, 9, 9, 12, 14, 20, 25, 28, 33, 34, 33, 32, 31, 31, 28, 24, 20, 19, 18, 17}
         };
 
+        int[] neueWerte = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+
         TempOverWeek t = new TempOverWeek(temperaturesOverTheWeek);
         t.minMaxTempWoche();
         t.avgTempTag(3);
         t.avgTempWoche();
         System.out.println(t.temperaturesOverTheWeek[0][0]);
-        t.setVal("Montag", 0, 7);
-        System.out.println(t.temperaturesOverTheWeek[0][0]);
+        t.setVal("Dienstag", 0, 7);
+        System.out.println(t.temperaturesOverTheWeek[1][0]);
+        t.setVals("Montag", neueWerte);
+        for (int stunde = 0; stunde < 24; stunde++) {
+            System.out.print(t.temperaturesOverTheWeek[0][stunde] + " -> ");
+        }
     }
 }
